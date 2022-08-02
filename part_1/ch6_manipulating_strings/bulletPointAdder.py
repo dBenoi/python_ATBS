d60f6e095e8ff1c1853765b18d17f22e924ac557
dbenoi <david.j.benoit1@gmail.com>
2022-08-01 22:21:46 -0500

    Update bulletPointAdder.py

--- part_1/ch6_manipulating_strings/bulletPointAdder.py
+++ part_1/ch6_manipulating_strings/bulletPointAdder.py
@@ -2,3 +2,14 @@
 # bulletPointAdder.py adds bullet points to the start 
 # of each line of text on the clipboard
 
+import pyperclip
+text = pyperclip.paste()
+
+# TODO separate lines and add stars
+lines = text.split('\n')
+for i in range(len(lines)): # loop through all indexes in 'lines'
+    lines[i] = '*' + lines[i]
+text = '\n'.join(lines)
+
+pyperclip.copy(text)
+
